1449f61ed7a2b49909aa343659ada560eafe0987
shafik-soweb <shafiksoweb191@gmail.com>
2025-03-20 06:51:25 +0600

    genesis

---
+++ src/views/Unlock/UnlockView.vue
@@ -0,0 +1,24 @@
+<script>
+import CsPinStep from '../../components/CsPinStep.vue';
+import CsSteps from '../../components/CsSteps.vue';
+
+import UnlockStepIndex from './UnlockStepIndex.vue';
+import UnlockStepSynchronization from './UnlockStepSynchronization.vue';
+
+export default {
+  components: {
+    CsSteps,
+  },
+  steps: {
+    index: UnlockStepIndex,
+    synchronization: UnlockStepSynchronization,
+    pin: CsPinStep,
+  },
+};
+</script>
+
+<template>
+  <CsSteps
+    :steps="$options.steps"
+  />
+</template>
